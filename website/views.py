d2bdf205b373a29b41f96d0d199ecfc528537167
“kevin” <kevinle1878@gmail.com>
2024-09-16 16:45:28 -0700

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    #request is the http link 
+
+    #if request is a POST method
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        # Authenticate from django
+        user = authenticate(request, username = username, password = password)
+
+        if user:
+            #from django
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request,"User does not exist")
+            return redirect('home')
+    return render(request, 'home.html', {})
+
+    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
